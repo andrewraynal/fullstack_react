a5a6ee20037548f63b8445164aabeaa805446685
andrewraynal <andrewraynal@gmail.com>
2017-11-01 14:38:34 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+// Route Handler
+
+// app = Express App to register this route handler with
+// get = Request method
+// / = Watch fo requests trying to access '/'
+// req = Object representing the incoming request
+// res = Object representing the outgoing response
+// res.send({hi: 'there'}) = Immediately send JSON back to who ever made the request
+
+app.get('/', (req, res) => {
+  res.send({
+    hi: 'there'
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(5000);
