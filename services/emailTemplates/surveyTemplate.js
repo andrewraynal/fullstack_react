76e3e2b67584a47a44ead5591398253082a5836b
andrewraynal <andrewraynal@gmail.com>
2017-11-17 11:16:59 -0500

    better email temp

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -4,19 +4,34 @@ module.exports = survey => {
   return `
     <html>
       <body>
-        <div style="text-align: center;">
-          <h3>I'd like your input!</h3>
-          <p>Please answer the following question:</p>
-          <p>${survey.body}</p>
-          <div style="width: 75px; height: 45px; margin: 5px auto; text-align: center;">
-            <a href="${keys.redirectDomain}/api/surveys/${
-    survey.id
-  }/yes">Yes</a>
-          </div>
-          <div style="width: 75px; height: 45px; margin: 5px auto; text-align: center;">
-            <a href="${keys.redirectDomain}/api/surveys/${survey.id}/no">No</a>
-          </div>
-        </div>
+        <table style="width: 440px; height: 440px; text-align: center; font-family: Arial, Helvetica, sans-serif; border: 1px solid #eee; background-color: #F2F2F2; color: #4F4F4F; margin: 0 auto;">
+          <tr style="width: 100%; height: 20%;">
+            <td>
+              <h2>I would like your feedback!</h2>
+            </td>
+          </tr>
+          <tr style="width: 100%; height: 10%;">
+            <td>
+              <h4>Please answer the following question ... </h4>
+            </td>
+          </tr>
+          <tr style="width: 100%; height: auto;">
+            <td style="width: 100%; border-top: 1px solid #fff; overflow: hidden;">
+              <p>${survey.body}
+                  <div>
+                    <a style="text-decoration: none;" href="${
+                      keys.redirectDomain
+                    }/api/surveys/${survey.id}/yes">Yes</a>
+                  </div>
+                  <div>
+                    <a style="text-decoration: none;" href='"${
+                      keys.redirectDomain
+                    }/api/surveys/${survey.id}/no"'>No</a>
+                  </div>
+              </p>
+            </td>
+          </tr>
+        </table>
       </body>
     </html>
   `;
